0502129bc8882fec057c62c4d71cd04dcb3d3894
RCaracuel <rosa.caracuel@gmail.com>
2020-01-30 16:28:18 +0100

    Añade mensaje con nombre y apellidos

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -21,7 +21,7 @@ public class ClaseString {
         
         // Obtener el char de una posicion concreta
         System.out.println("char de la posicion 5: " + s.charAt(5));
-       
+        System.out.println("Rosa Caracuel Calderón");
     }
     
 }
